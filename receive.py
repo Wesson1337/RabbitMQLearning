10647f3b4b0fa773176623dee6583e8fffec34ff
wesson <vinokurov.filipp1@yandex.ru>
2023-06-14 12:39:10 +0300

    .

--- receive.py
+++ receive.py
@@ -1,15 +1,17 @@
 import pika
 
+exchange_name = "logs"
+
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
 
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
-channel.queue_bind(exchange='logs', queue=queue_name)
+channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key="black")
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
--- send.py
+++ send.py
@@ -1,14 +1,16 @@
 import pika
 import sys
 
+exchange_name = "logs"
+
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
+channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
 
 message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-channel.basic_publish(exchange='logs', routing_key='', body=message)
+channel.basic_publish(exchange=exchange_name, routing_key='', body=message)
 print(" [x] Sent %r" % message)
 
 connection.close()
